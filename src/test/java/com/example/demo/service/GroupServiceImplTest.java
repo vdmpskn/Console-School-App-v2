0a518e7fb6ec8fde36397b16103f7f458ef71561
vdmpskn <vadim.piskun.dp@gmail.com>
2023-06-13 15:17:10 +0300

    delete imports

--- src/test/java/com/example/demo/service/GroupServiceImplTest.java
+++ src/test/java/com/example/demo/service/GroupServiceImplTest.java
@@ -6,12 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
